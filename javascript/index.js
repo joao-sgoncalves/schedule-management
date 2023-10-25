7c121b6cecf95efa75e8b89e39ba8f5436c39af6
João Gonçalves <joao.sgoncalves.97@gmail.com>
2023-10-25 20:52:49 +0100

    ✨ Added classroom CSV reading

--- javascript/index.js
+++ javascript/index.js
@@ -1,9 +1,47 @@
-const scheduleInput = document.getElementById("schedule-input");
+// TODO: Move to configurations!!
+const fieldSeparator = ';';
+const lineSeparator = '\r\n';
 
-scheduleInput.addEventListener("change", getSchedule, false);
+const roomsInput = document.getElementById("rooms-input");
 
-function getSchedule() {
-  const file = scheduleInput.files[0];
+roomsInput.addEventListener("change", handleFileChange, false);
 
-  console.log(file);
+function handleFileChange(input) {
+  const file = input.target.files[0];
+  const reader = new FileReader();
+
+  reader.onload = (event) => {
+    const result = event.target.result;
+    const classrooms = getFileData(result);
+
+    console.log(classrooms);
+  };
+
+  reader.readAsText(file);
 }
+
+function getFileData(content) {
+  const lines = content.split(lineSeparator);
+
+  const headerLine = lines[0];
+  const headerNames = headerLine.split(fieldSeparator);
+  
+  const dataLines = lines.slice(1);
+  const data = [];
+
+  dataLines.forEach((line) => {
+    const fields = line.split(fieldSeparator);
+
+    const obj = headerNames.reduce((obj, name, index) => {
+      obj[name] = fields[index];
+      return obj;
+    }, {});
+
+    data.push(obj);
+  });
+
+  return data;
+}
+
+// visualization in many criteria optimization
+// dizer que tem 50 sobrelotações, mostrar ao passar com o rato por cima
