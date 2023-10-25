2e155dfedead019bfadf56df806904e374ab6a37
João Gonçalves <joao.sgoncalves.97@gmail.com>
2023-10-25 19:00:32 +0100

    ✨ Read CSV file from input

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,9 @@
-console.log("hello world");
+const scheduleInput = document.getElementById("schedule-input");
+
+scheduleInput.addEventListener("change", getSchedule, false);
+
+function getSchedule() {
+  const file = scheduleInput.files[0];
+
+  console.log(file);
+}
