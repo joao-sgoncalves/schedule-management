7468b629676aed2c76a74667baa096e04bc75816
João Gonçalves <joao.goncalves@vwds.pt>
2023-12-29 16:36:04 +0000

    ✨ Added autocomplete to CodeMirror editor

--- js/editor.js
+++ js/editor.js
@@ -1,9 +1,27 @@
-// const codemirrorTest = CodeMirror.fromTextArea(document.getElementById('codemirror-test'), {
-//   mode: 'javascript',
-//   tabSize: 2,
-//   lineNumbers: true,
-//   allowDropFileTypes: ['text/javascript'],
-// });
+const codemirrorTest = CodeMirror.fromTextArea(document.getElementById('codemirror-test'), {
+  mode: 'javascript',
+  tabSize: 2,
+  lineNumbers: true,
+  extraKeys: { "Ctrl-Space": "autocomplete" },
+  allowDropFileTypes: ['text/javascript'],
+  hintOptions: {
+    hint: CodeMirror.hint.javascript,
+    completeSingle: false,
+  },
+  lint: true,
+});
+
+codemirrorTest.on('keyup', (cm, event) => {
+  const completionActive = cm.state.completionActive;
+  const excludedKeys = ['Enter', 'Escape'];
+
+  if (!completionActive && !excludedKeys.includes(event.key)) {
+    cm.showHint({
+      hint: CodeMirror.hint.javascript,
+      completeSingle: false,
+    });
+  }
+});
 
 const codemirrorTable = new Tabulator('#codemirror-table', {
   columns: [
@@ -17,11 +35,19 @@ const codemirrorTable = new Tabulator('#codemirror-table', {
           value: 'Default',
           mode: 'javascript',
           tabSize: 2,
+          extraKeys: { "Ctrl-Space": "autocomplete" },
           allowDropFileTypes: ['text/javascript'],
           autofocus: true,
+          hintOptions: {
+            alignWithWord: true,
+          },
         });
 
         codemirrorEditor.setSize('100%', '100%');
+
+        codemirrorEditor.on('keyup', (cm, event) => {
+          console.log(cm, event);
+        });
 
         return codemirrorEditor.getWrapperElement();
       },
